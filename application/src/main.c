688f3f091f146adcfe19bf984f1aa473db1c0adc
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2026-01-13 14:47:34 +0100

    Added file system support

--- application/src/main.c
+++ application/src/main.c
@@ -1,20 +1,38 @@
+#include <errno.h>
+#include <string.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/fs/littlefs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/storage/flash_map.h>
 
 LOG_MODULE_REGISTER(g2l_thing, LOG_LEVEL_INF);
 
-static int cmd_status(const struct shell* shell, size_t argc, char** argv) {
-    ARG_UNUSED(argc);
-    ARG_UNUSED(argv);
+#define FS_MOUNT_POINT "/lfs"
 
-    shell_print(shell, "uptime: %lld ms", k_uptime_get());
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+static struct fs_mount_t lfs_storage_mnt = {
+    .type = FS_LITTLEFS,
+    .fs_data = &storage,
+    .storage_dev = (void*)FIXED_PARTITION_ID(storage_partition),
+    .mnt_point = FS_MOUNT_POINT,
+};
+
+static int storage_init(void) {
+    int rc = fs_mount(&lfs_storage_mnt);
+
+    if (rc != 0) {
+        LOG_ERR("Error mounting storage (%d)", rc);
+        return rc;
+    }
+
+    LOG_INF("Storage mounted at %s", FS_MOUNT_POINT);
     return 0;
 }
 
-SHELL_CMD_REGISTER(status, NULL, "Print device status", cmd_status);
-
 int main(void) {
+    storage_init();
     LOG_INF("Application started");
 
     while (1) {
